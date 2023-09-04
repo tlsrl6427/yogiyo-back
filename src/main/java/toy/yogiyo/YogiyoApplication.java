b8a9b68a6b17a333b4a9103a3958dd2168ceddaf
skatn <87296704+skatn@users.noreply.github.com>
2023-09-05 00:30:28 +0900

    first commit

---
+++ src/main/java/toy/yogiyo/YogiyoApplication.java
@@ -0,0 +1,13 @@
+package toy.yogiyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogiyoApplication.class, args);
+    }
+
+}

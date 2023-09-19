2786432949b13e5f5909bd86b9159437f4b74392
tlsrl6427 <tlsr6427@naver.com>
2023-09-19 22:42:37 +0900

    Feat: Security Config add

---
+++ src/main/java/toy/yogiyo/common/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package toy.yogiyo.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import toy.yogiyo.common.security.jwt.JwtExceptionHandlerFilter;
+import toy.yogiyo.common.security.jwt.JwtFilter;
+import toy.yogiyo.common.security.jwt.JwtProvider;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/**").permitAll()
+                    .antMatchers(HttpMethod.POST, "/**").authenticated()
+                    .antMatchers(HttpMethod.PATCH, "/**").authenticated()
+                    .antMatchers(HttpMethod.DELETE, "/**").authenticated()
+                    .antMatchers("/member/join", "/login").permitAll()
+                .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(new JwtExceptionHandlerFilter(), JwtFilter.class)
+                .build();
+    }
+}

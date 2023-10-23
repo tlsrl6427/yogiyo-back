23f42085f91bcd8aa29caf4283bfc10d3d1f0ec4
tlsrl6427 <tlsr6427@naver.com>
2023-10-23 22:04:10 +0900

    Update: MemberAddress Entity & DTO 위도, 경도 추가

--- src/main/java/toy/yogiyo/core/Address/domain/MemberAddress.java
+++ src/main/java/toy/yogiyo/core/Address/domain/MemberAddress.java
@@ -1,5 +1,6 @@
 package toy.yogiyo.core.Address.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,15 +23,22 @@ public class MemberAddress {
     private AddressType addressType;
     private String nickname;
 
+    private Double longitude;
+    private Double latitude;
+
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @Builder
-    public MemberAddress(Address address, AddressType addressType, String nickname, Member member) {
+    public MemberAddress(Long id, Address address, AddressType addressType, String nickname, Double longitude, Double latitude, Member member) {
+        this.id = id;
         this.address = address;
         this.addressType = addressType;
         this.nickname = nickname;
+        this.longitude = longitude;
+        this.latitude = latitude;
         this.member = member;
     }
 }

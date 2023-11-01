78b6896af2a34c954dbe63202cf0ed9a83db85e8
skatn <87296704+skatn@users.noreply.github.com>
2023-11-01 17:14:07 +0900

    Fix: CorsConfig allowedOrigins -> allowedOriginPatterns

--- src/main/java/toy/yogiyo/common/config/CorsConfig.java
+++ src/main/java/toy/yogiyo/common/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true)
